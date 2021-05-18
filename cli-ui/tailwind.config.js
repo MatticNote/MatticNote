32bf807808140fad64dfe943c8b8a0d60cfef01a
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-05-18 21:02:27 +0900

    [Design] add button design

--- cli-ui/tailwind.config.js
+++ cli-ui/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
       '../client/src/**/*.html',
@@ -5,7 +7,12 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      }
+    },
   },
   variants: {
     extend: {},
