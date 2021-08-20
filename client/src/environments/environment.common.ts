42401e22bad69e089205355f3f08c0ec923f98e9
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-08-20 15:31:16 +0900

    default trusted domain

---
+++ client/src/environments/environment.common.ts
@@ -0,0 +1,13 @@
+export const environmentCommon = {
+  defaultTrustedDomain: [
+    'youtube.com',
+    'youtu.be',
+    'google.com',
+    // todo: Add google family domains, Example: google.co.jp, google.co.uk ...
+    'google.co.jp',
+    'apple.com',
+    'github.com',
+    'discord.com',
+    'discord.gg',
+  ]
+}
