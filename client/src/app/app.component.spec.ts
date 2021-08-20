c8bfab862c5ef874107db32fe7ee92999c14da83
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-08-20 14:55:32 +0900

    Frontend hotfix

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'matticnote-cli'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('matticnote-cli');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
