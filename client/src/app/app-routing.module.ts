7c29c980548b60c921922d21d12ea3e922eea034
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-08-20 14:52:26 +0900

    Frontend base

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./page/index/index.module').then(m => m.IndexModule),
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page/not-found/not-found.module').then(m => m.NotFoundModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/service/service-root.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SessionService} from "./session/session.service";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    SessionService,
+  ]
+})
+export class ServiceRootModule { }
