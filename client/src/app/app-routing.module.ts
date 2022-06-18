32fc6889d35c6db114cedb8f8f10e5bc08ff56e8
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-06-18 21:56:09 +0900

    Proxy & frontend pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule),
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
