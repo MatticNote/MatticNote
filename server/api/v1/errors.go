e60f773d47d56cef1ce7a87a0009b9e8c5e90b7c
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-06-19 21:43:04 +0900

    Refactor

--- server/api/v1/errors.go
+++ server/api/v1/errors.go
@@ -9,30 +9,25 @@ type (
 	}
 )
 
-func apiBadRequest(c *fiber.Ctx, detail string) error {
-	return c.Status(fiber.StatusBadRequest).JSON(apiErrorJson{
-		Code:   "BAD_REQUEST",
+func apiError(c *fiber.Ctx, status int, code, detail string) error {
+	return c.Status(status).JSON(apiErrorJson{
+		Code:   code,
 		Detail: detail,
 	})
+}
+
+func apiBadRequest(c *fiber.Ctx, detail string) error {
+	return apiError(c, fiber.StatusBadRequest, "BAD_REQUEST", detail)
 }
 
 func apiUnauthorized(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusUnauthorized).JSON(apiErrorJson{
-		Code:   "UNAUTHORIZED",
-		Detail: "Invalid credentials.",
-	})
+	return apiError(c, fiber.StatusUnauthorized, "UNAUTHORIZED", "Invalid credentials.")
 }
 
 func apiNotFound(c *fiber.Ctx, detail string) error {
-	return c.Status(fiber.StatusNotFound).JSON(apiErrorJson{
-		Code:   "NOT_FOUND",
-		Detail: detail,
-	})
+	return apiError(c, fiber.StatusNotFound, "NOT_FOUND", detail)
 }
 
 func apiGone(c *fiber.Ctx, detail string) error {
-	return c.Status(fiber.StatusGone).JSON(apiErrorJson{
-		Code:   "GONE",
-		Detail: detail,
-	})
+	return apiError(c, fiber.StatusGone, "GONE", detail)
 }
