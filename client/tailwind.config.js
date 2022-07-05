6ed440da6f975f7b40cf404df49a412869e801d9
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-07-05 19:51:25 +0900

    Setup angular & tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    '../template/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
