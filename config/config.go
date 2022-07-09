0994d2faac459ac43e483c0cf501ada2d191ca38
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-07-09 15:33:41 +0900

    Validate config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/pelletier/go-toml"
 	"io/ioutil"
 )
@@ -16,6 +17,11 @@ func LoadConfig() error {
 	var cfg MNConfig
 
 	err = toml.Unmarshal(file, &cfg)
+	if err != nil {
+		return err
+	}
+
+	err = validator.New().Struct(cfg)
 	if err != nil {
 		return err
 	}
