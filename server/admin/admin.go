5422a1acfd5cdb09d17ab7fb6e31834bacbc3357
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-07-12 16:55:20 +0900

    Machine metrics in admin section

--- server/admin/admin.go
+++ server/admin/admin.go
@@ -1,9 +1,17 @@
 package admin
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/MatticNote/MatticNote/internal"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+)
 
 func ConfigureRoute(r fiber.Router) {
 	r.Get("/", adminDashboard)
+	r.Get("/metrics", monitor.New(monitor.Config{
+		Title: fmt.Sprintf("MatticNote %s", internal.GetSysVersion()),
+	}))
 }
 
 func adminDashboard(c *fiber.Ctx) error {
